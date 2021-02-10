e2faa6e0fcda4eb828e7c5f94c87ff4cbe8ff776
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-02-09 21:48:37 -0300

    Taking screenshots from the tests.

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -1,11 +1,27 @@
 package core;
 
+import static core.DriverFactory.getDriver;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
 	
+	@Rule
+	public TestName testName = new TestName();
+	
 	@After
-	public void finaliza(){
+	public void finaliza() throws IOException {
+		TakesScreenshot screenshot = (TakesScreenshot) getDriver();
+		File file = screenshot.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("src" + File.separator + "main" + File.separator + "resources" + 
+				File.separator + "screenshots" + File.separator + testName.getMethodName() + ".jpg"));
+		
 		DriverFactory.killDriver();
 	}
 
